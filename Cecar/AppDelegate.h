730870f8a6e622b631699e0475414194f68ac011
Cecilie Haugstvedt <haugstvedt@gmail.com>
2012-01-12 13:22:10 +0100

    Initial Commit

---
+++ Cecar/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Cecar
+//
+//  Created by Cecilie Haugstvedt on 12.01.12.
+//  Copyright (c) 2012 NTNU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Cecar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cecar
+//
+//  Created by Cecilie Haugstvedt on 12.01.12.
+//  Copyright (c) 2012 NTNU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
